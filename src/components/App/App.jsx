b6e690ebbe57b7411ced000415696f7cd2f10a1d
Joshua Duenow <joshjduenow@gmail.com>
2023-11-24 08:47:30 -0600

    hard coded images

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,12 @@ function App() {
 
         <p>The gallery goes here!</p>
         <img src="images/goat_small.jpg"/>
+        <img style = {{width: 150, height: 150}} src="images/bucket_mac.jpeg"/>
+        <img style = {{width: 150, height: 150}} src="images/green_mac.jpeg"/>
+        <img style = {{width: 150, height: 150}} src="images/sheet_pan_thick.jpeg"/>
+        <img style = {{width: 150, height: 150}} src="images/sheet_pans_mac.jpeg"/>
+        <img style = {{width: 150, height: 150}} src="images/shredded_cheese.jpeg"/>
+        <img style = {{width: 150, height: 150}} src="images/singles_mac.jpeg"/>
       </div>
     );
 }
